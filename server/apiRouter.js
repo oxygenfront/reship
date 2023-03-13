4a2907dc33b71b1e28fc5dbc62a9eb272ea8eacb
Denis <bestloveapples@gmail.com>
2023-03-13 15:32:50 +0300

    backend commmit

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -3,6 +3,13 @@ import apiPostController from "./apiPostController.js";
 
 const router = Router();
 
-router.post('/getProducts', apiPostController.getProducts)
+router.post('/registration', apiPostController.registration)
+router.post('/auth', apiPostController.auth)
+router.get('/getProducts', apiPostController.getProducts)
+router.get('/getUser', apiPostController.getUser)
+router.post('/changePassword', apiPostController.changePassword)
+router.post('/changeEmail', apiPostController.changeEmail)
+router.get('/activateEmail', apiPostController.activateEmail)
+router.post('/recoveryPassword', apiPostController.recoveryPassword)
 
 export default router;

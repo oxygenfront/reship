71906a82e42bab0a232214f6d3bd8721c2e586a2
Denis <bestloveapples@gmail.com>
2023-03-17 01:49:23 +0300

    bek upd

--- server/index.js
+++ server/index.js
@@ -15,6 +15,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', apiRouter)
 
+app.use('/api', (request, response) => {
+    response.status(404).json({'api_error': "Метод не найден или указана не полная информация.", 'method': request.originalUrl.split('/')[2]})
+})
+
+app.use((request, response) => {
+    response.status(404).json({'error': "Страница не найдена."})
+})
+
 app.listen(PORT, () => {
     console.log(`SERVER STARTED ON ${PORT}`);
 });

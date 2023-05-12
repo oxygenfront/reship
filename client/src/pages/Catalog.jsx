1905b2b6e4af16c5119cfda26042ab60a546ae66
oxygen23 <markyurkulskiy@gmail.com>
2023-05-12 11:41:40 +0400

    catalog

--- client/src/pages/Catalog.jsx
+++ client/src/pages/Catalog.jsx
@@ -1,170 +1,248 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Card, Skeleton } from '../components'
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Card, Skeleton } from '../components';
 import {
   selectFilter,
   setChoosenCategorie,
   setSearchValue,
-} from '../redux/slices/fiterSlice'
-import { fetchFullItem } from '../redux/slices/fullItemSlice'
-import { selectItemsData } from '../redux/slices/itemsSlice'
+} from '../redux/slices/fiterSlice';
+import { fetchFullItem } from '../redux/slices/fullItemSlice';
+import { selectItemsData } from '../redux/slices/itemsSlice';
+import { RangeSlider, InputGroup, InputNumber } from 'rsuite';
 
 const Catalog = () => {
-  const dispatch = useDispatch()
-  const { items, status } = useSelector(selectItemsData)
+  const dispatch = useDispatch();
+  const { items, status } = useSelector(selectItemsData);
 
-  const { choosenCategorie, searchValue } = useSelector(selectFilter)
+  const { choosenCategorie, searchValue } = useSelector(selectFilter);
 
   const onChangeCategory = useCallback((sort) => {
-    dispatch(setChoosenCategorie(sort))
-  }, [])
-  const set = new Set()
+    dispatch(setChoosenCategorie(sort));
+  }, []);
+  const set = new Set();
 
   if (status === 'success') {
-    items.map((item) => set.add(item.category))
+    items.map((item) => set.add(item.category));
   }
-  const categories = [...set]
+  const categories = [...set];
+
+  const [rangeValue, setRangeValue] = useState([2000, 12000]);
 
   return (
-    <section className="catalog">
-      <div className="container catalog__container">
-        <div className="catalog__supheader">
-          <h1 className="catalog__title">Добро пожаловать в Каталог!</h1>
-          <div className="search-section catalog-section">
-            <div className=" catalog-section__container">
-              <div className="search-section__search-block catalog-section-search__search-block">
+    <section className='catalog'>
+      <div className='catalog__container container'>
+        <div className='catalog__title'>Мышки</div>
+        <div className='catalog__wrapper'>
+          <div className='catalog__sort'>
+            <div className='catalog__sort-block'>
+              <div className='catalog__sort_title'>Категория</div>
+              <label
+                htmlFor='category1'
+                className='catalog__sort_checkbox-name'
+              >
+                Беспроводные
                 <input
-                  value={searchValue}
-                  onChange={(e) => dispatch(setSearchValue(e.target.value))}
-                  type="text"
-                  placeholder="Поиск товара"
-                  className="search-section__search-item"
+                  id='category1'
+                  type='checkbox'
+                  className='catalog__sort_checkbox'
                 />
-                <button className="search-section__search-block_glass">
-                  <i className="fa-solid fa-magnifying-glass"></i>
+              </label>
+              <label
+                htmlFor='category2'
+                className='catalog__sort_checkbox-name'
+              >
+                Проводные
+                <input
+                  id='category2'
+                  type='checkbox'
+                  className='catalog__sort_checkbox'
+                />
+              </label>
+              <label
+                htmlFor='category3'
+                className='catalog__sort_checkbox-name'
+              >
+                С подсветкой
+                <input
+                  id='category3'
+                  type='checkbox'
+                  className='catalog__sort_checkbox'
+                />
+              </label>
+            </div>
+            <div className='catalog__sort-block'>
+              <div className='catalog__sort_title'>Бренды</div>
+              <label
+                htmlFor='category4'
+                className='catalog__sort_checkbox-name'
+              >
+                Razer
+                <input
+                  id='category4'
+                  type='checkbox'
+                  className='catalog__sort_checkbox'
+                />
+              </label>
+              <label
+                htmlFor='category5'
+                className='catalog__sort_checkbox-name'
+              >
+                Logitech
+                <input
+                  id='category5'
+                  type='checkbox'
+                  className='catalog__sort_checkbox'
+                />
+              </label>
+              <label
+                htmlFor='category6'
+                className='catalog__sort_checkbox-name'
+              >
+                Varmilo
+                <input
+                  id='category6'
+                  type='checkbox'
+                  className='catalog__sort_checkbox'
+                />
+              </label>
+            </div>
+            <div className='catalog__sort-block'>
+              <div className='catalog__sort_title'>Категория</div>
+              <label
+                htmlFor='category7'
+                className='catalog__sort_checkbox-name'
+              >
+                Оптическая
+                <input
+                  id='category7'
+                  type='checkbox'
+                  className='catalog__sort_checkbox'
+                />
+              </label>
+            </div>
+            <div className='catalog__sort-block mb73'>
+              <div className='catalog__sort_title'>Цена</div>
+              <RangeSlider
+                progress
+                value={rangeValue}
+                min={1000}
+                max={30000}
+                onChange={(value) => {
+                  setRangeValue(value);
+                }}
+              />
+              <InputGroup>
+                <InputNumber
+                  max={30000}
+                  value={rangeValue[0]}
+                  onChange={(nextValue) => {
+                    const [start, end] = rangeValue;
+                    setRangeValue([Number(nextValue), end]);
+                  }}
+                />
+                <InputNumber
+                  max={30000}
+                  value={rangeValue[1]}
+                  onChange={(nextValue) => {
+                    const [start, end] = rangeValue;
+                    setRangeValue([start, Number(nextValue)]);
+                  }}
+                />
+              </InputGroup>
+            </div>
+            <div className='catalog__sort-block'>
+              <label
+                htmlFor='category8'
+                className='catalog__sort_checkbox-name'
+              >
+                В наличии
+                <input
+                  id='category8'
+                  type='checkbox'
+                  className='catalog__sort_checkbox'
+                />
+              </label>
+              <label
+                htmlFor='category9'
+                className='catalog__sort_checkbox-name'
+              >
+                Сос скидкой
+                <input
+                  id='category9'
+                  type='checkbox'
+                  className='catalog__sort_checkbox'
+                />
+              </label>
+            </div>
+            <div className='catalog__sort-block_buttons'>
+              <button className='catalog__sort-block_buttons-item blue'>
+                Применить
+              </button>
+              <button className='catalog__sort-block_buttons-item white'>
+                Сбросить
+              </button>
+            </div>
+          </div>
+
+          <div className='catalog__main'>
+            <div className='catalog__main_filters'>
+              <div className='catalog__main_filters_popular'>
+                <span>С подсветкой</span>
+                <span>Razer</span>
+                <span>Проводные</span>
+                <div>Logitech</div>
+                <div>Беспроводные</div>
+              </div>
+
+              <div className='catalog__main_filters-main'>
+                <span>Сортировка</span>
+                <button className='catalog__main_filters-main_popup'>
+                  <img src='../assets/img/arrows-sort.png' alt='' />
+                </button>
+                <button className='catalog__main_filters-main_row'>
+                  <img src='../assets/img/filter-row-main.png' alt='' />
+                </button>
+                <button className='catalog__main_filters-main_grid'>
+                  <img src='../assets/img/filter-grid-main.png' alt='' />
                 </button>
               </div>
             </div>
-          </div>
-          <div className="main-catalog__buttons buttons__10">
-            <button
-              onClick={() => onChangeCategory('')}
-              className="main-catalog__buttons-item buttons__10-item catalog-section__buttons-item"
-              id="catalog"
-            >
-              <span>Каталог</span>
-            </button>
 
-            <button
-              onClick={() => onChangeCategory('новинки')}
-              className="main-catalog__buttons-item buttons__10-item catalog-section__buttons-item"
-              id="news"
-            >
-              <span>Новинки</span>
-            </button>
-
-            <button
-              onClick={() => onChangeCategory('акции')}
-              className="main-catalog__buttons-item buttons__10-item
-							catalog-section__buttons-item"
-              id="action"
-            >
-              <span>Акции</span>
-            </button>
-
-            <button
-              onClick={() => onChangeCategory('лидеры продаж')}
-              className="main-catalog__buttons-item buttons__10-item
-							catalog-section__buttons-item"
-              id="leaders"
-            >
-              <span>Лидеры продаж</span>
-            </button>
-          </div>
-        </div>
-
-        <div
-          className={
-            choosenCategorie === 'акции'
-              ? 'catalog__items-block'
-              : 'catalog__block'
-          }
-        >
-          {choosenCategorie === 'акции'
-            ? items
-                .filter((item) => {
-                  if (item.old_price !== item.price) {
-                    return true
-                  } else {
-                    return false
-                  }
-                })
-
-                .filter((item) => {
-                  if (
-                    item.name.toLowerCase().includes(searchValue.toLowerCase())
-                  ) {
-                    return true
-                  } else {
-                    return false
-                  }
-                })
-                .map((item) => (
-                  <Card
-                    key={item.id}
-                    id={item.id}
-                    name={item.name}
-                    old_price={item.old_price}
-                    price={item.price}
-                  ></Card>
-                ))
-            : categories.map((categorie, index) => (
-                <>
-                  <div className="catalog__suptitle" id={categorie}>
-                    <span>{categorie}</span>
+            <div className='catalog__main_wrapper'>
+              <div className='catalog__main_item'>
+                <div className='catalog__main_item_left'>
+                  <img src='../assets/img/nigger.jpg' alt='' />
+                </div>
+                <div className='catalog__main_item_mid'>
+                  <div className='catalog__main_item_mid-title'>
+                    Афроамериканец Уфува
                   </div>
-                  <div key={index} className="catalog__items-block">
-                    {status === 'loading'
-                      ? [...new Array(3)].map((_, index) => (
-                          <Skeleton key={index}></Skeleton>
-                        ))
-                      : items
-                          .filter((item) => {
-                            if (item.category === categorie) {
-                              return true
-                            } else {
-                              return false
-                            }
-                          })
-                          .filter((item) => {
-                            if (
-                              item.name
-                                .toLowerCase()
-                                .includes(searchValue.toLowerCase())
-                            ) {
-                              return true
-                            } else {
-                              return false
-                            }
-                          })
-
-                          .map((item) => (
-                            <Card
-                              key={item.id}
-                              id={item.id}
-                              name={item.name}
-                              old_price={item.old_price}
-                              price={item.price}
-                              image={item.image_link}
-                            ></Card>
-                          ))}
+                  <div className='catalog__main_item_mid-subtitle'>
+                    Черные работают лучше. <br /> Хороший экземпляр. Хлопок
+                    собирает быстро, если достать плеть, ускоряется вдвое.
                   </div>
-                </>
-              ))}
+                </div>
+                <div className='catalog__main_item_right'>
+                  <div className='catalog__main_item_right-price'>
+                    от 11 111 руб
+                  </div>
+                  <div className='catalog__main_item_right-rating'>
+                    <img src='../assets/img/star-review.png' alt='' />
+                    <span>4.5</span>
+                    <span>5 отзывов</span>
+                  </div>
+
+                  <button className='catalog__main_item_right-add'>
+                    В корзину
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Catalog
+export default Catalog;

aa9a6a9084f80ee84a785e4722e334760e05fb7e
Mark Yurkulkskiy <markyurkulskiy@gmail.com>
2023-07-05 23:46:31 +0400

    fullItemSlider

--- client/src/components/FullItemSlider.jsx
+++ client/src/components/FullItemSlider.jsx
@@ -1,49 +1,77 @@
-import React, { useState } from 'react';
-import { Navigation, Thumbs } from 'swiper';
+import React, { useState, useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Thumbs } from 'swiper';
+import 'swiper/swiper-bundle.css';
+
 const FullItemSlider = ({ id, image }) => {
   const parsedImage = JSON.parse(image);
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const swiperRef = useRef(null);
+
+  const handleSlideChange = (swiper) => {
+    const currentIndex = swiper.realIndex;
+    setCurrentSlide(currentIndex);
+  };
+
+  const goToPreviousSlide = () => {
+    if (swiperRef.current) {
+      const swiperInstance = swiperRef.current.swiper;
+      swiperInstance.slidePrev();
+      setCurrentSlide(swiperInstance.realIndex);
+    }
+  };
+
+  const goToNextSlide = () => {
+    if (swiperRef.current) {
+      console.log(swiperRef.current)
+      
+      const swiperInstance = swiperRef.current.swiper;
+      swiperInstance.slideNext();
+      setCurrentSlide(swiperInstance.realIndex);
+    }
+  };
+
   return (
     <>
-      <Swiper
-        className='fullitem__card-slider_big-wrapper'
-        modules={[Thumbs]}
-        spaceBetween={60}
-        slidesPerView={1}
-        speed={1000}
-        onSwiper={setThumbsSwiper}
-      >
-        {parsedImage.map((image) => (
-          <SwiperSlide key={image} className='fullitem__card-slider_big-item'>
-            <img src={image} alt='' />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      {thumbsSwiper && (
+      <div className='fullitem__card-slider_big-wrapper'>
+        <img
+          src={parsedImage[currentSlide]}
+          alt='Большая картинка'
+          className='fullitem__card-slider_big-item'
+        />
+      </div>
+      <div className='fullitem__card-slider_small-wrapper_overflow'>
         <Swiper
           className='fullitem__card-slider_small-wrapper'
           modules={[Navigation, Thumbs]}
-          navigation
-          loop
-          centeredSlides
           slideToClickedSlide
-          thumbs={{ swiper: thumbsSwiper }}
-          slidesPerView={3}
-          spaceBetween={15}
+          slidesPerView='auto'
+          loop
+          spaceBetween={20}
           speed={1000}
+          onSlideChange={handleSlideChange}
+          ref={swiperRef}
         >
-          {parsedImage.map((image) => (
+          {parsedImage.map((image, index) => (
             <SwiperSlide
-              key={image}
+              key={index}
               className='fullitem__card-slider_small-item'
             >
-              <img src={image} alt='' />
+              <img src={image} alt={`Маленькая картинка`} />
             </SwiperSlide>
           ))}
         </Swiper>
-      )}
+      </div>
+      <button
+        onClick={goToPreviousSlide}
+        className='fullitem__card-slider_small-wrapper_button-prev'
+      ></button>
+      <button
+        onClick={goToNextSlide}
+        className='fullitem__card-slider_small-wrapper_button-next'
+      ></button>
     </>
   );
 };
+
 export default FullItemSlider;

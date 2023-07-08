4c4f1fd0614be9bd8712e52abfaf5f6c1df7db09
Mark Yurkulkskiy <markyurkulskiy@gmail.com>
2023-07-08 16:08:48 +0400

    little fix bugs

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,49 +1,67 @@
-import classNames from 'classnames'
-import { Fragment, memo, useCallback, useEffect, useRef, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
+import classNames from 'classnames';
+import {
+  Fragment,
+  memo,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 import {
   logout,
   selectIsAuth,
   selectUserData,
-} from '../../redux/slices/authSlice'
+} from '../../redux/slices/authSlice';
 import {
   selectFilter,
   setChoosenCategorie,
   setSearchValue,
-} from '../../redux/slices/fiterSlice'
-import styles from './Menu.module.sass'
-import { Dialog, Transition } from '@headlessui/react'
-import { selectItemsData } from '../../redux/slices/itemsSlice'
-import Card from '../Card/Card'
-import { selectCart } from '../../redux/slices/cartSlice'
-import { Menu as DropDown } from '@headlessui/react'
-import { getFavoritesFromLs } from '../../utils/getFavoritesFromLs'
-import { addFavorite, selectFavorites } from '../../redux/slices/favoriteSlice'
-import Modal from './Modal'
+} from '../../redux/slices/fiterSlice';
+import styles from './Menu.module.sass';
+import { Dialog, Transition } from '@headlessui/react';
+import { selectItemsData } from '../../redux/slices/itemsSlice';
+import Card from '../Card/Card';
+import { selectCart } from '../../redux/slices/cartSlice';
+import { Menu as DropDown } from '@headlessui/react';
+import { getFavoritesFromLs } from '../../utils/getFavoritesFromLs';
+import { addFavorite, selectFavorites } from '../../redux/slices/favoriteSlice';
+import Modal from './Modal';
 
 const Menu = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const isMounted = useRef(false)
-  const isAuth = useSelector(selectIsAuth)
-  const { data, status } = useSelector(selectUserData)
-  const { searchValue } = useSelector(selectFilter)
-  const { favorites } = useSelector(selectFavorites)
-  const { items, itemsStatus = status } = useSelector(selectItemsData)
-  const { cartItems } = useSelector(selectCart)
-  const theme = useSelector((state) => state.theme)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isMounted = useRef(false);
+  const isAuth = useSelector(selectIsAuth);
+  const { data, status } = useSelector(selectUserData);
+  const { searchValue } = useSelector(selectFilter);
+  const { favorites } = useSelector(selectFavorites);
+  const { items, itemsStatus = status } = useSelector(selectItemsData);
+  const { cartItems } = useSelector(selectCart);
+  const theme = useSelector((state) => state.theme);
 
-  const totalCount = cartItems.reduce((sum, item) => sum + item.count, 0)
+  const totalCount = cartItems.reduce((sum, item) => sum + item.count, 0);
 
-  const [isOpen, setIsOpen] = useState(false)
-  const [searchOpen, setSearchOpen] = useState(false)
-  const [scrollTop, setScrollTop] = useState(0)
+  const [isOpen, setIsOpen] = useState(false);
 
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-  const [showBurger, setShowBurger] = useState(false)
+  const refInput = useRef(null);
+  const [focusedInput, setFocusedInput] = useState(false);
+  const handleFocus = () => {
+    setFocusedInput(true);
+  };
 
-  const favoriteCount = favorites ? favorites.length : null
+  const handleBlur = () => {
+    setFocusedInput(false);
+  };
+
+  const [searchOpen, setSearchOpen] = useState(false);
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [showBurger, setShowBurger] = useState(false);
+
+  const favoriteCount = favorites ? favorites.length : null;
 
   const stylesBurgerMenuWrapperNoShow = {
     width: '100vw',
@@ -53,7 +71,7 @@ const Menu = () => {
     top: '100px',
     zIndex: '0',
     background: 'none',
-  }
+  };
   const stylesBurgerMenuWrapperShow = {
     width: '100vw',
     height: '100vh',
@@ -63,7 +81,7 @@ const Menu = () => {
     zIndex: '0',
     background: 'rgba(0, 0, 0, .6)',
     transition: 'all .2s linear',
-  }
+  };
   const stylesBurgerMenuListNoShow = {
     transition: 'all .2s linear',
     position: 'absolute',
@@ -73,7 +91,7 @@ const Menu = () => {
     right: '-250px',
     height: '85vh',
     width: '247px',
-  }
+  };
   const stylesBurgerMenuListShow = {
     transition: 'all .2s linear',
     position: 'absolute',
@@ -83,45 +101,51 @@ const Menu = () => {
     right: '35px',
     height: '84vh',
     width: '247px',
-  }
+  };
 
   const onClickLogout = () => {
     if (window.confirm('Вы действительно хотите выйти?')) {
-      dispatch(logout())
-      window.localStorage.removeItem('token')
-      return navigate('/')
+      dispatch(logout());
+      window.localStorage.removeItem('token');
+      return navigate('/');
     }
-  }
+  };
   const onClickClear = () => {
-    dispatch(setSearchValue(''))
-  }
+    dispatch(setSearchValue(''));
+  };
 
   useEffect(() => {
     function handleResize() {
-      setWindowWidth(window.innerWidth)
+      setWindowWidth(window.innerWidth);
     }
 
-    window.addEventListener('resize', handleResize)
+    window.addEventListener('resize', handleResize);
 
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   useEffect(() => {
     if (isMounted.current) {
-      const json = JSON.stringify(cartItems)
-      const favJson = JSON.stringify(favorites)
+      const json = JSON.stringify(cartItems);
+      const favJson = JSON.stringify(favorites);
 
-      localStorage.setItem('cart', json)
-      localStorage.setItem('favorites', favJson)
+      localStorage.setItem('cart', json);
+      localStorage.setItem('favorites', favJson);
     }
-    isMounted.current = true
-  }, [cartItems, favorites])
+    isMounted.current = true;
+  }, [cartItems, favorites]);
 
   useEffect(() => {
-    const handleScroll = () => setScrollTop(window.scrollY)
-    window.addEventListener('scroll', handleScroll)
+    const handleScroll = () => setScrollTop(window.scrollY);
+    window.addEventListener('scroll', handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  function handelFocesedInput() {
+    setFocusedInput(true);
+    refInput.current.focus();
+  }
   return (
     <div
       className={styles.search_section}
@@ -134,25 +158,57 @@ const Menu = () => {
         )}
       >
         <>
-          <Link to="/" className={styles.search_section__logo}>
-            <img src="../assets/img/logo.svg" alt="logo" />
+          <Link to='/' className={styles.search_section__logo}>
+            <img src='../assets/img/logo.svg' alt='logo' />
           </Link>
 
           <Link
+            style={
+              focusedInput
+                ? { width: '0px', fontSize: '0', border: 'unset' }
+                : null
+            }
             onClick={() => setIsOpen(true)}
             className={styles.search_section__catalog}
           >
             <span>Каталог</span>
 
-            <div className={styles.search_section__catalog_arrow}></div>
+            <div
+              className={styles.search_section__catalog_arrow}
+              style={
+                focusedInput
+                  ? { width: '0px', fontSize: '0', border: 'unset' }
+                  : null
+              }
+            ></div>
           </Link>
         </>
 
-        <div className={styles.search_section__search_block}>
+        <div
+          className={styles.search_section__search_block}
+          style={
+            !focusedInput && windowWidth <= 450
+              ? {
+                  width: '13%',
+                  border: 'white',
+                }
+              : null
+          }
+        >
           <input
+            ref={refInput}
+            onFocus={handleFocus}
+            // onBlur={handleBlur}
             value={searchValue}
-            type="text"
-            placeholder="Поиск по каталогу"
+            type='text'
+            placeholder={
+              focusedInput && windowWidth <= 450
+                ? 'Поиск по каталогу'
+                : windowWidth > 450
+                ? 'Поиск  по каталогу'
+                : null
+            }
+            style={!focusedInput && windowWidth <= 450 ? { margin: 0 } : null}
             className={styles.search_section__search_item}
             onChange={(e) => dispatch(setSearchValue(e.target.value))}
           />
@@ -180,27 +236,44 @@ const Menu = () => {
               </div>
             </div>
           </Dialog> */}
-          <Link
-            to="/catalog"
-            className={styles.search_section__search_block_glass}
-          >
-            <i className="fa-solid fa-magnifying-glass"></i>
-          </Link>
+          {!focusedInput && windowWidth <= 450 ? (
+            <button
+              className={styles.search_section__search_block_glass}
+              onClick={handelFocesedInput}
+            >
+              <i className='fa-solid fa-magnifying-glass'></i>
+            </button>
+          ) : windowWidth <= 450 ? (
+              <Link
+                onClick={() => setFocusedInput(!focusedInput)}
+              to='/catalog'
+              className={styles.search_section__search_block_glass}
+            >
+              <i className='fa-solid fa-magnifying-glass'></i>
+            </Link>
+          ) : (
+            <Link
+              to='/catalog'
+              className={styles.search_section__search_block_glass}
+            >
+              <i className='fa-solid fa-magnifying-glass'></i>
+            </Link>
+          )}
         </div>
         {windowWidth > 991 ? (
           <div className={styles.search_section_links}>
-            <Link to="/personal/favorites">
+            <Link to='/personal/favorites'>
               {theme === 'light' ? (
                 <img
                   className={styles.search_section_links_heart}
-                  src="../assets/img/heart.svg"
-                  alt="favorites"
+                  src='../assets/img/heart.svg'
+                  alt='favorites'
                 />
               ) : (
                 <img
                   className={styles.search_section_links_heart}
-                  src="../assets/img/heart-white.svg"
-                  alt="favorites"
+                  src='../assets/img/heart-white.svg'
+                  alt='favorites'
                   width={35}
                 />
               )}
@@ -211,19 +284,19 @@ const Menu = () => {
                 </span>
               ) : null}
             </Link>
-            <Link to="/cart">
+            <Link to='/cart'>
               {theme === 'light' ? (
                 <img
                   className={styles.search_section_links_cart}
-                  src="../assets/img/cart.svg"
-                  alt="cart"
+                  src='../assets/img/cart.svg'
+                  alt='cart'
                   width={33}
                 />
               ) : (
                 <img
                   className={styles.search_section_links_cart}
-                  src="../assets/img/cart-white.svg"
-                  alt="cart"
+                  src='../assets/img/cart-white.svg'
+                  alt='cart'
                   width={33}
                 />
               )}
@@ -241,7 +314,7 @@ const Menu = () => {
             <>
               {windowWidth > 991 ? (
                 <div className={styles.search_section__profile_block}>
-                  <Link to="/personal">
+                  <Link to='/personal'>
                     {data.first_name ? (
                       <>
                         <p>
@@ -257,7 +330,7 @@ const Menu = () => {
                   </Link>
 
                   <div className={styles.menu_wrapper}>
-                    <DropDown as="div" className={styles.menu}>
+                    <DropDown as='div' className={styles.menu}>
                       <div>
                         <DropDown.Button className={styles.menu_button}>
                           <div className={styles.search_section__catalog_block}>
@@ -271,38 +344,38 @@ const Menu = () => {
                       </div>
                       <Transition
                         as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
+                        enter='transition ease-out duration-100'
+                        enterFrom='transform opacity-0 scale-95'
+                        enterTo='transform opacity-100 scale-100'
+                        leave='transition ease-in duration-75'
+                        leaveFrom='transform opacity-100 scale-100'
+                        leaveTo='transform opacity-0 scale-95'
                       >
                         <DropDown.Items className={styles.menu_items}>
                           <div className={styles.menu_items_wrapper}>
                             <DropDown.Item className={styles.menu_items_item}>
-                              <Link to="/personal">Личный кабинет</Link>
+                              <Link to='/personal'>Личный кабинет</Link>
                             </DropDown.Item>
                             <DropDown.Item
-                              to="/personal/favorites"
+                              to='/personal/favorites'
                               className={styles.menu_items_item}
                             >
                               <Link>Избранные</Link>
                             </DropDown.Item>
                             <DropDown.Item
-                              to="/settings"
+                              to='/settings'
                               className={styles.menu_items_item}
                             >
                               <Link>Настройки</Link>
                             </DropDown.Item>
                             <DropDown.Item
-                              to="/orders"
+                              to='/orders'
                               className={styles.menu_items_item}
                             >
                               <Link>Мои Заказы</Link>
                             </DropDown.Item>
                             <DropDown.Item
-                              to="/cart"
+                              to='/cart'
                               className={styles.menu_items_item}
                             >
                               <Link>Корзина</Link>
@@ -335,7 +408,7 @@ const Menu = () => {
         ) : (
           <>
             {windowWidth > 991 ? (
-              <Link to="/login" className={styles.search_section__login_block}>
+              <Link to='/login' className={styles.search_section__login_block}>
                 <div className={styles.search_section__login_item}>Войти</div>
               </Link>
             ) : (
@@ -374,44 +447,44 @@ const Menu = () => {
                 >
                   <Link
                     onClick={() => setShowBurger(!showBurger)}
-                    to="/personal"
+                    to='/personal'
                     className={styles.search_section_burger_items_item}
                   >
                     Профиль
                   </Link>
-                  <hr className="hr" />
+                  <hr className='hr' />
                   <Link
                     onClick={() => setShowBurger(!showBurger)}
-                    to="/cart"
+                    to='/cart'
                     className={styles.search_section_burger_items_item}
                   >
                     Моя корзина
                   </Link>
-                  <hr className="hr" />
+                  <hr className='hr' />
                   <Link
                     onClick={() => setShowBurger(!showBurger)}
-                    to="/personal/favorites"
+                    to='/personal/favorites'
                     className={styles.search_section_burger_items_item}
                   >
                     Избранное
                   </Link>
-                  <hr className="hr" />
+                  <hr className='hr' />
                   <Link
                     onClick={() => setShowBurger(!showBurger)}
-                    to="/settings"
+                    to='/settings'
                     className={styles.search_section_burger_items_item}
                   >
                     Настройки
                   </Link>
-                  <hr className="hr" />
+                  <hr className='hr' />
                   <Link
-                    to="/orders"
+                    to='/orders'
                     onClick={() => setShowBurger(!showBurger)}
                     className={styles.search_section_burger_items_item}
                   >
                     Мои заказы
                   </Link>
-                  <hr className="hr" />
+                  <hr className='hr' />
                   <li
                     onClick={(() => setShowBurger(!showBurger), onClickLogout)}
                     className={styles.search_section_burger_items_item}
@@ -431,27 +504,27 @@ const Menu = () => {
               >
                 <Link
                   onClick={() => setShowBurger(!showBurger)}
-                  to="/login"
+                  to='/login'
                   className={styles.search_section_burger_items_item}
                 >
                   Войти
                 </Link>
-                <hr className="hr" />
+                <hr className='hr' />
                 <Link
                   onClick={() => setShowBurger(!showBurger)}
-                  to="/register"
+                  to='/register'
                   className={styles.search_section_burger_items_item}
                 >
                   Зарегистрироваться
                 </Link>
-                <hr className="hr" />
+                <hr className='hr' />
                 <li
                   onClick={() => setShowBurger(!showBurger)}
                   className={styles.search_section_burger_items_item}
                 >
                   Мои избранные
                 </li>
-                <hr className="hr" />
+                <hr className='hr' />
                 <li
                   onClick={() => setShowBurger(!showBurger)}
                   className={styles.search_section_burger_items_item}
@@ -469,7 +542,7 @@ const Menu = () => {
         ) : null}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;

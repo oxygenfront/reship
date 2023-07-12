d57b336cf9fa19411dda0987a2fdf7f33d1247e7
Mark Yurkulkskiy <markyurkulskiy@gmail.com>
2023-07-12 18:19:01 +0400

    errors register/auth with little bugs

---
+++ client/src/components/Error/Error.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import styles from './Error.module.sass';
+import classnames from 'classnames';
+function Error({ children, isErrorProp }) {
+  
+  const [isError, setIsError] = useState(isErrorProp);
+  
+  useEffect(() => {
+    setIsError(isErrorProp)
+  }, [isErrorProp])
+
+  console.log(isError)
+  
+  
+  return (
+    <div
+      onClick={() => setIsError(false)}
+      className={
+        isError ? classnames(styles.error, styles.active) : styles.error
+      }
+    >
+      <div className={styles.error_wrapper}>
+        <span>{children}</span>
+        <div>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Error;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,96 +1,106 @@
-import React, { useRef, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, Navigate } from 'react-router-dom'
-import { HiOutlineEye, HiOutlineEyeOff } from 'react-icons/hi'
-import { fetchAuth, fetchAuthMe, selectIsAuth } from '../redux/slices/authSlice'
+import React, { useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, Navigate } from 'react-router-dom';
+import { HiOutlineEye, HiOutlineEyeOff } from 'react-icons/hi';
+import {
+  fetchAuth,
+  fetchAuthMe,
+  selectIsAuth,
+} from '../redux/slices/authSlice';
+import Error from '../components/Error/Error';
 
 const Login = () => {
-  const dispatch = useDispatch()
-  const isAuth = useSelector(selectIsAuth)
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
   const [authForm, setAuthForm] = useState({
     email: '',
     password: '',
-  })
-  const [showPassword, setShowPassword] = useState(false)
-  const [hasFocus, setHasFocus] = useState(false)
-
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const [hasFocus, setHasFocus] = useState(false);
+  const [isError, setIsError] = useState(false);
   const handleChildFocus = () => {
-    setHasFocus(true)
-  }
+    setHasFocus(true);
+  };
 
   const handleChildBlur = () => {
-    setHasFocus(false)
-  }
-  const inputsRef = useRef([])
+    setHasFocus(false);
+  };
+  const inputsRef = useRef([]);
   const handleKeyDown = (event, index) => {
     if (event.key === 'Enter') {
       if (index < inputsRef.current.length - 1) {
-        inputsRef.current[index + 1].focus()
+        inputsRef.current[index + 1].focus();
       } else {
-        sendForm(event)
+        sendForm(event);
       }
     }
-  }
+  };
 
   const updateAuthForm = (e) => {
     setAuthForm({
       ...authForm,
       [e.target.name]: e.target.value,
-    })
-  }
+    });
+  };
 
   async function sendForm(e) {
-    e.preventDefault()
-    const data = await dispatch(fetchAuth(authForm))
-
+    e.preventDefault();
+    const data = await dispatch(fetchAuth(authForm));
+    
     if (!data.payload) {
-      return alert('Не удалось авторизоваться')
+      return setIsError(true);
     }
 
-    if ('token' in data.payload) {
-      window.localStorage.setItem('token', data.payload.token)
-      dispatch(fetchAuthMe(data.payload.token))
+    if ( 'token' in data.payload && isError) {
+      window.localStorage.setItem('token', data.payload.token);
+      dispatch(fetchAuthMe(data.payload.token));
     }
 
     setAuthForm({
       email: '',
       password: '',
-    })
+    });
   }
 
   if (isAuth) {
-    return <Navigate to="/" />
+    return <Navigate to='/' />;
   }
 
   return (
     <div>
-      <section className="auth">
-        <div className="container auth__container">
-          <div className="auth_wrapper">
-            <h1 className="auth__title">Войти</h1>
-            <hr className="hr"></hr>
-            <form className="auth__form" action="">
+      <section className='auth'>
+        <div className='container auth__container'>
+          <div className='auth_wrapper'>
+            <Error isErrorProp={isError}>Неверный логин или пароль</Error>
+            <h1 className='auth__title'>Войти</h1>
+            <hr className='hr'></hr>
+            <form className='auth__form' action=''>
               <input
                 autoFocus
-                className="auth__form-input"
-                name="email"
+                className={
+                  !isError ? 'auth__form-input' : 'auth__form-input active'
+                }
+                name='email'
                 value={authForm.email}
                 onChange={updateAuthForm}
-                type="text"
-                placeholder="Адрес электронной почты"
+                type='text'
+                placeholder='Адрес электронной почты'
                 ref={(ref) => (inputsRef.current[0] = ref)}
                 onKeyDown={(event) => handleKeyDown(event, 0)}
               />
               <div
-                className="auth__form-input"
+                className={
+                  !isError ? 'auth__form-input' : 'auth__form-input active'
+                }
                 style={{ border: hasFocus ? '2px solid #0540F2' : null }}
               >
                 <input
-                  name="password"
+                  name='password'
                   value={authForm.password}
                   onChange={updateAuthForm}
                   type={showPassword ? 'text' : 'password'}
-                  placeholder="Пароль"
+                  placeholder='Пароль'
                   ref={(ref) => (inputsRef.current[1] = ref)}
                   onKeyDown={(event) => handleKeyDown(event, 1)}
                   onFocus={handleChildFocus}
@@ -109,17 +119,17 @@ const Login = () => {
                 )}
               </div>
             </form>
-            <div className="auth__links">
+            <div className='auth__links'>
               Новый пользователь?
-              <Link to="/register" className="auth__reg">
+              <Link to='/register' className='auth__reg'>
                 Зарегистрируйтесь
               </Link>
             </div>
-            <div className="auth__bottom">
-              <Link to="/forgot" className="auth__reg">
+            <div className='auth__bottom'>
+              <Link to='/forgot' className='auth__reg'>
                 Забыл пароль
               </Link>
-              <button className="auth__button" onClick={sendForm}>
+              <button className='auth__button' onClick={sendForm}>
                 Выполнить вход
               </button>
             </div>
@@ -127,7 +137,7 @@ const Login = () => {
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;

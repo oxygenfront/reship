30f9bb5a4f3a3d076b1b5531503ccf81709c27d3
kvartycs <kvartyclash@gmail.com>
2023-08-13 18:08:59 +0700

    skeletons admin

--- client/src/components/AdminOrder/AdminOrders.jsx
+++ client/src/components/AdminOrder/AdminOrders.jsx
@@ -1,31 +1,35 @@
-import React, { useEffect } from 'react';
-import { AdminOrder } from '../../components';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchAllOrders, selectAdminData } from '../../redux/slices/adminSlice';
-import { selectUserData } from '../../redux/slices/authSlice';
-import { Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { AdminOrder } from '../../components'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAllOrders, selectAdminData } from '../../redux/slices/adminSlice'
+import { selectUserData } from '../../redux/slices/authSlice'
+import { Navigate } from 'react-router-dom'
+import { Skeleton } from '../../components'
 
 const AdminOrders = () => {
-  const dispatch = useDispatch();
-  const token = localStorage.getItem('token');
-  const { orders, ordersStatus } = useSelector(selectAdminData);
-  const { data, status } = useSelector(selectUserData);
+  const dispatch = useDispatch()
+  const token = localStorage.getItem('token')
+  const { orders, ordersStatus } = useSelector(selectAdminData)
+  const { data, status } = useSelector(selectUserData)
   useEffect(() => {
-    dispatch(fetchAllOrders({ token }));
-  }, []);
+    dispatch(fetchAllOrders({ token }))
+  }, [])
   if (status === 'success' && data !== null) {
     if (data.admin !== 1) {
-      return <Navigate to='/'></Navigate>;
+      return <Navigate to="/"></Navigate>
     }
   }
   return (
     <>
-      {ordersStatus === 'success' &&
-        orders.map((item) => (
-          <AdminOrder key={item.id} obj={item}></AdminOrder>
-        ))}
+      {ordersStatus === 'success'
+        ? orders.map((item) => (
+            <AdminOrder key={item.id} obj={item}></AdminOrder>
+          ))
+        : [...new Array(8)].map((item, index) => (
+            <Skeleton view={'grid'} key={index}></Skeleton>
+          ))}
     </>
-  );
-};
+  )
+}
 
-export default AdminOrders;
+export default AdminOrders
--- client/src/components/AdminPromocode/AdminPromocodes.jsx
+++ client/src/components/AdminPromocode/AdminPromocodes.jsx
@@ -1,42 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, Navigate } from 'react-router-dom';
-import { AdminPromocode } from '../../components';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, Navigate } from 'react-router-dom'
+import { AdminPromocode } from '../../components'
 import {
   fetchAllPromocodes,
   selectAdminData,
-} from '../../redux/slices/adminSlice';
-import { selectIsAuth, selectUserData } from '../../redux/slices/authSlice';
+} from '../../redux/slices/adminSlice'
+import { selectIsAuth, selectUserData } from '../../redux/slices/authSlice'
+import { Skeleton } from '../../components'
 
 const AdminPromocodes = () => {
-  const dispatch = useDispatch();
-  const token = localStorage.getItem('token');
-  const isAuth = useSelector(selectIsAuth);
-  const [isOpen, setIsOpen] = useState(false);
-  const { promocodes, status } = useSelector(selectAdminData);
-  const { data, userStatus = status } = useSelector(selectUserData);
+  const dispatch = useDispatch()
+  const token = localStorage.getItem('token')
+  const isAuth = useSelector(selectIsAuth)
+  const [isOpen, setIsOpen] = useState(false)
+  const { promocodes, status } = useSelector(selectAdminData)
+  const { data, userStatus = status } = useSelector(selectUserData)
   useEffect(() => {
-    dispatch(fetchAllPromocodes({ token }));
-  }, []);
+    dispatch(fetchAllPromocodes({ token }))
+  }, [])
   if (userStatus === 'success' && data !== null) {
     if (data.admin !== 1) {
-      return <Navigate to='/'></Navigate>;
+      return <Navigate to="/"></Navigate>
     }
   }
   return (
     <>
-      {promocodes &&
-        promocodes.map((item) => (
-          <AdminPromocode
-            key={item.id}
-            id={item.id}
-            promocode={item.promocode}
-            persent={item.persent}
-            date_end={item.date_end}
-          ></AdminPromocode>
-        ))}
+      {status === 'success'
+        ? promocodes.map((item) => (
+            <AdminPromocode
+              key={item.id}
+              id={item.id}
+              promocode={item.promocode}
+              persent={item.persent}
+              date_end={item.date_end}
+            ></AdminPromocode>
+          ))
+        : [...new Array(8)].map((item, index) => (
+            <Skeleton view={'grid'} key={index}></Skeleton>
+          ))}
     </>
-  );
-};
+  )
+}
 
-export default AdminPromocodes;
+export default AdminPromocodes
